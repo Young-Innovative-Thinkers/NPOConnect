7dcb6ded5cc13c59e79aa39f0c03b057d499c109
Matt Permentier <matt.permentier@gmail.com>
2024-03-17 21:43:42 +0100

    Add eslint rules

--- src/modules/Account/Account.js
+++ src/modules/Account/Account.js
@@ -1,9 +1,9 @@
-import { View, Text } from "react-native";
+import { View, Text } from 'react-native';
 
 export default function Account() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Account Screen</Text>
-    </View>
-  );
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Account Screen</Text>
+        </View>
+    );
 }
