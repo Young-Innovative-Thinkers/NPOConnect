609bafe896b5040ce1f2c81d2e701f369e33f2b6
thomasmartini <thomasmartini1999@gmail.com>
2024-03-19 14:08:58 +0100

    Added feed front-end basics

--- src/modules/Home/Home.js
+++ src/modules/Home/Home.js
@@ -1,9 +1,45 @@
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
+import { Avatar, Button, Card, Divider, Text } from 'react-native-paper';
+
+const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+
 
 export default function Home() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-        </View>
+    return(
+        <ScrollView>
+        <Card>
+        <Card.Title title="Boom omgevallen" subtitle="Recht op mijn auto" left={LeftContent} />
+        <Card.Content>
+          <Text variant="bodyMedium">Kom uit mijn huis ligt er ineens een boom op mijn auto</Text>
+        </Card.Content>
+        <Card.Cover source={{ uri: 'https://picsum.photos/750' }} />
+        <Card.Actions>
+          <Button>Read</Button>
+        </Card.Actions>
+      </Card>
+      <Divider />
+      <Card>
+        <Card.Title title="Boom omgevallen" subtitle="Recht op mijn auto" left={LeftContent} />
+        <Card.Content>
+          <Text variant="bodyMedium">Kom uit mijn huis ligt er ineens een boom op mijn auto</Text>
+        </Card.Content>
+        <Card.Cover source={{ uri: 'https://picsum.photos/730' }} />
+        <Card.Actions>
+          <Button>Read</Button>
+        </Card.Actions>
+      </Card>
+      <Divider />
+      <Card>
+        <Card.Title title="Boom omgevallen" subtitle="Recht op mijn auto" left={LeftContent} />
+        <Card.Content>
+          <Text variant="bodyMedium">Kom uit mijn huis ligt er ineens een boom op mijn auto</Text>
+        </Card.Content>
+        <Card.Cover source={{ uri: 'https://picsum.photos/751' }} />
+        <Card.Actions>
+          <Button>Read</Button>
+        </Card.Actions>
+      </Card>
+      </ScrollView>
+      
     );
 }
