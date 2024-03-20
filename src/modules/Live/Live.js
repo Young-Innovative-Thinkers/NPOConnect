9e0efcf04191df16634b9f33defcaa8715672418
THOMASterPRO <57947885+THOMASterPRO@users.noreply.github.com>
2024-03-20 12:20:24 +0100

    Added live screen start

--- src/modules/Live/Live.js
+++ src/modules/Live/Live.js
@@ -1,9 +1,42 @@
-import { View, Text } from 'react-native';
+import { View, Text , StyleSheet, Image} from 'react-native';
+import React from 'react';
+import { TextInput } from 'react-native-paper';
 
 export default function Live() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Live Screen</Text>
+        <View style={styles.container}>
+            <Text style={styles.live}>Live: Newspresenter</Text>
+
+            <Image source={require('./img/auto.jpg')} style={styles.img} />
+
+            
         </View>
     );
+
+    
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 10,
+        marginLeft: 10,
+        
+    },
+    img: {
+        width: 410,
+        height: 600
+    },
+    live: {
+        fontSize: 20,
+        
+    },
+    bigBlue: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    red: {
+        color: 'red',
+    },
+});
