66a3118ecba72ad30f413461dbc3665df0a1ac50
Matt Permentier <matt.permentier@gmail.com>
2024-03-28 14:22:52 +0100

    Remove moderation pop up

--- src/modules/Home/Home.js
+++ src/modules/Home/Home.js
@@ -1,11 +1,9 @@
 import { View, Text } from 'react-native';
-import ModerationPopUp from './Components/ModerationPopUp';
 
 export default function Home() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <ModerationPopUp />
-            {/* <Text>Home Screen</Text> */}
+            <Text>Home Screen</Text>
         </View>
     );
 }
